20a5f6f5304f97d9d76050ee1f5b78c1ecec144f
Matthew Memme <mattmemme@me.com>
2021-09-18 16:53:22 -0400

    Designs index and student listings pages

--- components/StudentListItem.tsx
+++ components/StudentListItem.tsx
@@ -1,11 +1,10 @@
 import * as React from "react"
 import {
-  Badge,
-  Box,
+  Button,
   Flex,
   Image,
   LinkBox,
-  LinkOverlay,
+  Spacer,
   Text,
 } from "@chakra-ui/react"
 import { MdStar } from "react-icons/md"
@@ -20,25 +19,35 @@ export default function ListItem(props: StudentListItems) {
   console.log("customer:", customer)
 
   return (
-    <LinkBox p="5" borderWidth="1px" rounded="md">
-      <Flex align="baseline" mt={2}>
-        <Text
-          textTransform="uppercase"
-          fontSize="sm"
-          fontWeight="bold"
-          color="pink.800"
-        >
-          {"Username:" && customer.husername}
-          {"Email:" && customer.email}
+    <LinkBox p="5" borderWidth="1px" rounded="md" paddingTop={0}>
+      <Flex paddingTop={5} paddingBottom={2} spacing={5}>
+        <Image
+          height="45px"
+          objectFit="contain"
+          src="/avatar.png"
+          alt="avatar"
+          paddingRight={5}
+        />
+        <Text mt={2} fontSize="xl" fontWeight="bold" color="#2d2d2d">
+          {customer.name}
         </Text>
+        <Spacer />
+        <Button colorScheme="blue" size="sm">
+          <Link href={`/students/${customer._id}`}>Donate</Link>
+        </Button>
       </Flex>
-      <Link href={`/students/${customer._id}`} passHref>
-        <LinkOverlay>
-          <Text mt={2} fontSize="xl" fontWeight="semibold" lineHeight="short">
-            {customer.name}
-          </Text>
-        </LinkOverlay>
-      </Link>
+      
+      <Text fontSize="sm" fontWeight="bold" color="#2d2d2d">
+        Username: {customer.username}
+      </Text>
+      <Text fontSize="sm" fontWeight="bold" color="#2d2d2d">
+        Email: {customer.email}
+      </Text>  
+      <br/>
+      <Text fontSize="sm">
+        About: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </Text>
+      
     </LinkBox>
   )
 }
